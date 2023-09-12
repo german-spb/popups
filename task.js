e63d1433bf477adccecb73690ff5882acd0f9c0d
Герман <108299345+german-spb@users.noreply.github.com>
2023-09-12 21:49:37 +0300

    Update task.js

--- task.js
+++ task.js
@@ -1,17 +1,17 @@
 const mainWindow = document.getElementById('modal_main')
 const successWindow = document.getElementById('modal_success')
 mainWindow.className = 'modal modal_active'
+closeWindow = document.getElementsByClassName('modal__close modal__close_times')
 
-let closeWindow = document.getElementsByClassName('modal__close modal__close_times')[0]
-closeWindow.onclick = () => {
-    mainWindow.className = 'modal'
-}
 let showWindow = document.getElementsByClassName('btn btn_danger modal__close show-success')[0]
 showWindow.onclick = () => {
     successWindow.className = 'modal modal_active'
     mainWindow.className = 'modal'
 }
-closeWindow = document.getElementsByClassName('modal__close modal__close_times')[1]
-closeWindow.onclick = () => {
-    successWindow.className = 'modal'
+
+for (let i = 0; i <= 1; i++){
+    closeWindow[i].onclick = () => {
+        successWindow.className = 'modal'
+        mainWindow.className = 'modal'
+    }
 }
